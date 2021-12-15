93452c54494d14371f25c88bd58246c6f7785305
Chester <cheung319@gmail.com>
2021-12-14 23:12:58 -0500

    commit tree b288c5c44a866e502314f96d525a95e44e046904

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+const { headersArrayToObject } = require("nock/lib/common")
+
+function submitData(name, email){
+
+// const htmlBody = document.querySelector("body")
+
+const postData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+
+    body:JSON.stringify({
+        name,
+        email
+    }),
+}
+
+
+
+   return fetch("http://localhost:3000/users",postData)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (user){
+        document.body.innerHTML = user["id"]
+    })
+.catch(function (error){
+    document.body.innerHTML = error;
+})
+}
